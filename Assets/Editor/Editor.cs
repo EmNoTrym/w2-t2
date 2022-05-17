4930305008b9a72376305da2b56e05c2902e58b5
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-17 21:01:27 +0700

    update_points

--- Assets/Editor/Editor.cs
+++ Assets/Editor/Editor.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class Editor : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Editor : EditorWindow
+{ 
+
+    [MenuItem("Window/Change waypoint location")]
+    public static void ShowWindow()
     {
-        
+        GetWindow<Editor>("Change waypoint location");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        foreach (var obj in Selection.gameObjects)
+        {
+            float x = obj.transform.position.x;
+            float y = obj.transform.position.y;
+            float z = obj.transform.position.z;
+            GUILayout.Label(obj.name, EditorStyles.boldLabel);
+            x = EditorGUILayout.FloatField("X", x);
+            y = EditorGUILayout.FloatField("Y", y);
+            z = EditorGUILayout.FloatField("Z", z);
+            obj.transform.position = new Vector3(x, y, z);
+        }
     }
+
 }
